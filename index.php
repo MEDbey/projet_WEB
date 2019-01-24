324e8d9466a003e38ca3d97673cfc9a3ceb3aa56
MarwanJerbi <je.marwan@gmail.com>
2019-01-24 14:49:49 +0100

    projet web

---
+++ index.php
@@ -0,0 +1,20 @@
+﻿<?php
+	if(isset($_GET['controle']) & isset($_GET['action']) & isset($_GET['id'])){
+		$controle = $_GET['controle'];
+		require ('./controller/' . $controle . '.php');
+		$cont = new $controle();
+		$action= $_GET['action'];
+		$id=$_GET['id'];
+		$cont->{$action}($id); 
+	}
+	elseif (isset($_GET['controle']) & isset($_GET['action'])) {
+		$controle = $_GET['controle'];
+		require ('./controller/' . $controle . '.php');
+		$cont = new $controle();
+		$action= $_GET['action'];
+		$cont->{$action}(); 
+	}
+	else {
+	require("vue/layout/edt.html");
+	}
+?>
